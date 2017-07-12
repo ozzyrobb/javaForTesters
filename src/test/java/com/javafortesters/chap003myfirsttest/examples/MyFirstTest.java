4e13e3acbe5b15d5b08729312fda0136ddc8281e
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-07-12 21:13:06 +0100

    Add new assertion failure example

--- src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
+++ src/test/java/com/javafortesters/chap003myfirsttest/examples/MyFirstTest.java
@@ -2,9 +2,7 @@
  * Created by robb on 30/04/2017.
  */
 package com.javafortesters.chap003myfirsttest.examples;
-
 import org.junit.Test;
-
 import static org.junit.Assert.assertEquals;
 
 public class MyFirstTest {
@@ -13,6 +11,12 @@ public class MyFirstTest {
     public void canAddTwoPlusTwo() {
         int answer = 2+2;
         assertEquals("2+2=4", 4, answer);
+    }
+
+    @Test
+    public void canAddTwoPlusTwoAssertionError() {
+        int answer = 2+2;
+        assertEquals("2+2=4", 5, answer);
     }
 
     @Test
