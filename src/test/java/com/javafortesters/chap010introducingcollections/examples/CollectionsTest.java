6718edcab2ebb7f195a2404c36ef8f178b407bd0
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-08-24 07:35:34 +0100

    Comparison of for each, for, while and do...while

--- src/test/java/com/javafortesters/chap010introducingcollections/examples/CollectionsTest.java
+++ src/test/java/com/javafortesters/chap010introducingcollections/examples/CollectionsTest.java
@@ -51,4 +51,50 @@ public class CollectionsTest {
     // List<String> numbers0123 = new ArrayList<String>();
     // In the above declaration List is an interface which declares the type of Collection I am using.
     // ArrayList is the specific implementation of List that I am using.
+
+    @Test
+    public void daysOfTheWeek() {
+        String[] someDays = {"Tuesday", "Thursday", "Wednesday", "Monday", "Saturday", "Sunday", "Friday"};
+
+        List<String> days = Arrays.asList(someDays);
+
+        // With the for each loop, I can iterate over every item in the List and when I find "Monday" i will have to break out of the loop:
+
+        int forCount = 0;
+        for (String day : days) {
+            if (day.equals("Monday")) {
+                break;
+            }
+            forCount++;
+        }
+        assertEquals("Monday is at position 3", 3, forCount);
+
+        // With the for loop, I will iterate over the size of the List and break when I find "Monday":
+
+        int loopCount;
+        for (loopCount = 0; loopCount <= days.size(); loopCount++) {
+            if (days.get(loopCount).equals("Monday")) {
+                break;
+            }
+        }
+        assertEquals("Monday is at position 3", 3, loopCount);
+
+        // With the while loop, I can make the check for "Monday" the loop exit condition, so I only 'do' the body of the loop 'while' I have not found "Monday":
+
+        int count = 0;
+        while (!days.get(count).equals("Monday")) {
+            count++;
+        }
+        assertEquals("Monday is at position 3", 3, count);
+
+        // With the do...while loop, I need to set the index outside the valid boundary of the list because I increment it in the body of the loop.
+        // And again, I only 'do' the body of the loop 'while' I have not found "Monday":
+
+        int doCount = -1;
+        do {
+            doCount++;
+        } while (!days.get(doCount).equals("Monday"));
+        assertEquals("Monday is at position 3", 3, doCount);
+    }
 }
+
