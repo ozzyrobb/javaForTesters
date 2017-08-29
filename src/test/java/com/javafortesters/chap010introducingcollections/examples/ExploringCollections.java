0eb5bb0ac1a87f6bfa76a689e9af9f252b07ea82
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-08-29 06:59:25 +0100

    iterate over a collection; use methods clear, isEmpty, removeAll

--- src/test/java/com/javafortesters/chap010introducingcollections/examples/ExploringCollections.java
+++ src/test/java/com/javafortesters/chap010introducingcollections/examples/ExploringCollections.java
@@ -32,11 +32,48 @@ public class ExploringCollections {
         weekendDays.add("Sunday");
 
         daysOfWeek.addAll(workdays);
+        daysOfWeek.addAll(weekendDays);
+
+        for (String dayOfWeek : daysOfWeek) {
+            System.out.println(dayOfWeek);
+        }
 
         assertEquals("workdays contains 5 elements",5, workdays.size());
         assertTrue("daysOfWeek contains 5 elements", daysOfWeek.containsAll(workdays));
         assertFalse("weekendDays does not contain Funday",weekendDays.contains("Funday"));
         assertEquals("weekendDays contains 2 elements", 2, weekendDays.size());
         assertTrue("Bug fixed, Sunday is in the collection now", weekendDays.contains("Sunday"));
+
+        assertEquals("daysOfWeek contains 7 elements", 7, daysOfWeek.size());
+        daysOfWeek.clear();
+        assertEquals("daysOfWeek is now empty", 0, daysOfWeek.size());
+        assertTrue(daysOfWeek.isEmpty());
+    }
+
+    @Test
+    public void removeAll() {
+        List<String> workdays = new ArrayList<>();
+        List<String> daysOfWeek = new ArrayList<>();
+        List<String> weekendDays = new ArrayList<>();
+
+        workdays.add("Monday");
+        workdays.add("Tuesday");
+        workdays.add("Wednesday");
+        workdays.add("Thursday");
+        workdays.add("Friday");
+
+        weekendDays.add("Saturday");
+        weekendDays.add("Sunday");
+
+        daysOfWeek.addAll(workdays);
+        daysOfWeek.addAll(weekendDays);
+
+        assertEquals("daysOfWeek contains 7 elements", 7, daysOfWeek.size());
+
+        daysOfWeek.removeAll(weekendDays);
+
+        assertTrue(daysOfWeek.containsAll(workdays));
+        assertEquals("daysOfWeek contains 5 elements", 5, daysOfWeek.size());
+        assertFalse(daysOfWeek.containsAll(weekendDays));
     }
 }
