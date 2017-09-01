74ace1b6bc2a1b710c674f6050fe66427b96f72c
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-01 07:20:53 +0100

    Use put to add key,value pairs to a map

--- src/test/java/com/javafortesters/chap010introducingcollections/examples/ExploringCollections.java
+++ src/test/java/com/javafortesters/chap010introducingcollections/examples/ExploringCollections.java
@@ -292,4 +292,15 @@ public class ExploringCollections {
 
         assertEquals(1, workdays.size());
     }
+
+    @Test
+    public void putElementsInAMap() {
+        Map<String,String> map = new HashMap<>();
+
+        map.put("key1", "value1");
+        map.put("key2", "value2");
+        map.put("key3", "value3");
+
+        assertEquals(3, map.size());
+    }
 }
