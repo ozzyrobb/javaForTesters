52710aaa91e349dc51ea186dd82631effaf4cf52
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-02 18:00:44 +0100

    Use try catch to handle NullPointerException

--- src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExamplesTest.java
+++ src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExamplesTest.java
@@ -1,7 +1,28 @@
 package com.javafortesters.chap011exceptions.examples;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 /**
  * Created by robb on 02/09/2017.
  */
 public class ExceptionsExamplesTest {
+
+    @Test
+    public void throwANullPointerException() {
+        Integer age = null;
+        String ageAsString;
+
+        try {
+            ageAsString = age.toString();
+        } catch (NullPointerException e) {
+            age = 18;
+            ageAsString = age.toString();
+        }
+
+        String yourAge = "You are " + ageAsString + " years old";
+
+        assertEquals("You are 18 years old", yourAge);
+    }
 }
