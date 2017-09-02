5ab63707c875f0873d7a10780bf4b12d9cf53086
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-02 18:13:23 +0100

    Use exception as object

--- src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExamplesTest.java
+++ src/test/java/com/javafortesters/chap011exceptions/examples/ExceptionsExamplesTest.java
@@ -2,6 +2,8 @@ package com.javafortesters.chap011exceptions.examples;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+
 import static org.junit.Assert.assertEquals;
 
 /**
@@ -17,6 +19,10 @@ public class ExceptionsExamplesTest {
         try {
             ageAsString = age.toString();
         } catch (NullPointerException e) {
+            System.out.println("getMessage - " + e.getMessage());
+            System.out.println("getStackTrace - " + Arrays.toString(e.getStackTrace()));
+            System.out.println("printStackTrace");
+            e.printStackTrace();
             age = 18;
             ageAsString = age.toString();
         }
