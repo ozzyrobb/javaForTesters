fbe7709206612ca298cf36f89fef54ccd2672822
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-10 11:04:51 +0100

    Add new files for handling exception

---
+++ src/main/java/com/javafortesters/domainentities/exceptions/InvalidPassword.java
@@ -0,0 +1,7 @@
+package com.javafortesters.domainentities;
+
+/**
+ * Created by robb on 07/09/2017.
+ */
+public class InvalidPassword {
+}
---
+++ src/main/java/com/javafortesters/domainentities/exceptions/User.java
@@ -0,0 +1,7 @@
+package com.javafortesters.domainentities.exceptions;
+
+/**
+ * Created by robb on 10/09/2017.
+ */
+public class User {
+}
