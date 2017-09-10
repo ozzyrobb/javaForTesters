b1f84b982de2eaab73a68b9bad74ca5e068d1692
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-10 11:06:50 +0100

    Create duplicate User class with exception thrown to prevent refactoring of existing tests

--- src/main/java/com/javafortesters/domainentities/exceptions/User.java
+++ src/main/java/com/javafortesters/domainentities/exceptions/User.java
@@ -4,4 +4,42 @@ package com.javafortesters.domainentities.exceptions;
  * Created by robb on 10/09/2017.
  */
 public class User {
+    private String username;
+    private String password;
+
+    public User(){
+        this("username", "password", false);
+    }
+
+    private User(String username, String password, boolean b) {
+        // only call this because we don't want to throw the exception
+        this.username = username;
+        try{
+            setPassword(password);
+        }catch(InvalidPassword e){
+            throw new IllegalArgumentException(
+                    "Default password incorrect ", e);
+        }
+    }
+
+    public User(String username, String password) throws InvalidPassword{
+        this.username = username;
+        setPassword(password);
+    }
+
+    public void setPassword(String password) throws InvalidPassword {
+        if(password.length()<7){
+            throw new InvalidPassword("Password must be > 6 chars");
+        }
+
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
 }
