31e3ab26f13fc576975353c04481b45a6dcaddf9
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-11 07:34:34 +0100

    Exercise: Create @Test method which uses all of the asserts

---
+++ src/test/java/com/javafortesters/chap014junitexplored/exercises/JunitExploredExercisesTest.java
@@ -0,0 +1,28 @@
+package com.javafortesters.chap014junitexplored.exercises;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by robb on 11/09/2017.
+ */
+public class JunitExploredExercisesTest {
+
+    @Test
+    public void createTestMethodUsingAllAsserts() {
+        int rob = 5;
+        int[] robArray = {0,1,2};
+        int[] robNewArray = {0,1,2};
+        String bob = null;
+        int job = 5;
+        assertEquals("rob = 5", 5, rob);
+        assertTrue(rob == 5);
+        assertFalse(rob == 4);
+        assertArrayEquals(robArray, robNewArray);
+        assertNotNull(rob);
+        assertNull(bob);
+        assertNotSame(rob, bob);
+        assertSame(rob, job);
+    }
+}
