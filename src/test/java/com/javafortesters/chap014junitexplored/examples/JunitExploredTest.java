6c1bdc3d81329cb6c5395a95592383b835b8a9f3
Rob Brightwell <Rob.Brightwell@matchesfashion.com>
2017-09-11 07:11:36 +0100

    @Test @Rule @Before and @After

---
+++ src/test/java/com/javafortesters/chap014junitexplored/examples/JunitExploredTest.java
@@ -0,0 +1,47 @@
+package com.javafortesters.chap014junitexplored.examples;
+
+import com.javafortesters.domainentities.exceptions.InvalidPassword;
+import com.javafortesters.domainentities.exceptions.User;
+import org.junit.*;
+import org.junit.rules.ExpectedException;
+
+/**
+ * Created by robb on 11/09/2017.
+ */
+public class JunitExploredTest {
+
+    @Test(expected = InvalidPassword.class)
+    public void expectInvalidPasswordException() throws InvalidPassword {
+        User user = new User("username", "<6");
+    }
+
+    @Rule
+    public ExpectedException expected = ExpectedException.none();
+
+    @Test
+    public void invalidPasswordThrown() throws InvalidPassword {
+        expected.expect(InvalidPassword.class);
+        expected.expectMessage("> 6 chars");
+        User user = new User("username", ",6");
+    }
+
+    @BeforeClass
+    public static void runOncePerClassBeforeAnyTests() {
+        System.out.println("@BeforeClass run once before all tests");
+    }
+
+    @AfterClass
+    public static void runOncePerClassAfterAnyTests() {
+        System.out.println("@AfterClass run once after all tests");
+    }
+
+    @Before
+    public void runOncePerTestBeforeTest() {
+        System.out.println("@Before run once before each test");
+    }
+
+    @After
+    public void runOncePerTestAfterTest() {
+        System.out.println("@After run once after each test");
+    }
+}
