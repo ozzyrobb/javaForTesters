93b57204d47c9cd9e723080a36cd250b8bf324c9
Rob Brightwell <rob.brightwell@gmail.com>
2017-10-31 07:23:07 +0000

    Special characters

--- src/test/java/com/javafortesters/chap015stringsrevisited/examples/StringsRevisitedTest.java
+++ src/test/java/com/javafortesters/chap015stringsrevisited/examples/StringsRevisitedTest.java
@@ -1,5 +1,12 @@
 package com.javafortesters.chap015stringsrevisited.examples;
 
+import org.junit.Test;
+
 public class StringsRevisitedTest {
 
+    @Test
+    public void printUsingSpecialCharacters() {
+        System.out.println("Bob said \"hello\" to his cat's friend");
+        System.out.println("This is a single backslash \\");
+    }
 }
--- src/test/java/com/javafortesters/chap015stringsrevisited/exercises/StringsRevisitedExercisesTest.java
+++ src/test/java/com/javafortesters/chap015stringsrevisited/exercises/StringsRevisitedExercisesTest.java
@@ -1,4 +1,14 @@
 package com.javafortesters.chap015stringsrevisited.exercises;
 
+import org.junit.Test;
+
 public class StringsRevisitedExercisesTest {
+
+    @Test
+    public void experimentWithSpecialCharacters() {
+        System.out.println("I can put tabs \t into my strings");
+        System.out.println("And I can use the backspace \bcharacter");
+        System.out.println("I can separate text onto \na new line");
+        System.out.println("I can use a carriage return \rto return to the beginning of a line");
+    }
 }
