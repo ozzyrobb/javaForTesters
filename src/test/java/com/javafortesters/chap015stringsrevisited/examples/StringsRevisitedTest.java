e6d9e618edd9928386386da5d801f635510e7e6a
Rob Brightwell <rob.brightwell@gmail.com>
2017-11-01 07:01:46 +0000

    String concatenation

--- src/test/java/com/javafortesters/chap015stringsrevisited/examples/StringsRevisitedTest.java
+++ src/test/java/com/javafortesters/chap015stringsrevisited/examples/StringsRevisitedTest.java
@@ -2,11 +2,26 @@ package com.javafortesters.chap015stringsrevisited.examples;
 
 import org.junit.Test;
 
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class StringsRevisitedTest {
 
     @Test
     public void printUsingSpecialCharacters() {
         System.out.println("Bob said \"hello\" to his cat's friend");
         System.out.println("This is a single backslash \\");
+    }
+
+    @Test
+    public void stringConcatenation() {
+        String ps1 = "This is " + "String2";
+        assertThat(ps1, is("This is String2"));
+        String ps2 = "This is " + 4;
+        assertThat(ps2, is("This is 4"));
+
+        String thisIs = "This is ";
+        String s1 = thisIs.concat("String1");
+        assertThat(s1, is("This is String1"));
     }
 }
